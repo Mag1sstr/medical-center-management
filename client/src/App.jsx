7969fba4aeacc29cd4ae145e420188bcae12be7b
Daniil K <danilkarachev234@mail.ru>
2026-05-02 04:11:12 +0500

    update App

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,10 @@
-import { Routes, Route, Navigate } from 'react-router-dom';
-import Navbar from './components/Navbar';
-import PatientsPage from './pages/PatientsPage';
-import DoctorsPage from './pages/DoctorsPage';
-import AppointmentsPage from './pages/AppointmentsPage';
-import TreatmentsPage from './pages/TreatmentsPage';
-import AnalyticsPage from './pages/AnalyticsPage';
+import { Routes, Route, Navigate } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import PatientsPage from "./pages/PatientsPage";
+import DoctorsPage from "./pages/DoctorsPage";
+import AppointmentsPage from "./pages/AppointmentsPage";
+import TreatmentsPage from "./pages/TreatmentsPage";
+import AnalyticsPage from "./pages/AnalyticsPage";
 
 export default function App() {
   return (
@@ -17,7 +17,7 @@ export default function App() {
           <Route path="/appointments" element={<AppointmentsPage />} />
           <Route path="/treatments" element={<TreatmentsPage />} />
           <Route path="/analytics" element={<AnalyticsPage />} />
-          <Route path="*" element={<Navigate to="/" replace />} />
+          {/* <Route path="*" element={<Navigate to="/" replace />} /> */}
         </Routes>
       </main>
     </div>
